382e0e8dae161b56303c1669da7c23a11762a38d
IceChoco <dkfk2685@naver.com>
2022-02-19 11:30:22 +0900

    add Alogorithm: countDistinctSlices of codility Lesson 15

--- src/main/java/codility/Lesson15_Caterpillar_method/AbsDistinct/Solution.java
+++ src/main/java/codility/Lesson15_Caterpillar_method/AbsDistinct/Solution.java
@@ -1,9 +1,19 @@
 package codility.Lesson15_Caterpillar_method.AbsDistinct;
 
+import java.util.HashSet;
+
 public class Solution {
     public int solution(int[] A){
+        HashSet<Integer> hs = new HashSet<>();
+        for(int a:A){
+            hs.add(Math.abs(a));
+        }
 
+        return hs.size();
+    }
 
-        return 0;
+    public static void main(String[] args) {
+        int[] A = {-5,-3,-1,0,3,6};
+        System.out.println(new Solution().solution(A));
     }
 }
---
+++ src/main/java/codility/Lesson15_Caterpillar_method/CountDistinctSlices/Solution.java
@@ -0,0 +1,13 @@
+package codility.Lesson15_Caterpillar_method.CountDistinctSlices;
+
+public class Solution {
+    public int solution(int M, int[] A){
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
